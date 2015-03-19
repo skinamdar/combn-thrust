aa0e778b6e98372a166d949fbe7bb4b2e1e06844
Syeda Kauser Inamdar <skinamdar@ucdavis.edu>
2015-03-18 21:10:27 -0700

    Adding Jen's updates. WIP on calculating the position

--- thrustcombn.cpp
+++ thrustcombn.cpp
@@ -1,121 +1,188 @@
 // .cu
-// goal is to send numbers 1 to 5 to device.
-// mulitply by 2
-// send back to get basic program working
-
 #include <iostream>
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
-#include <thrust/generate.h>
 #include <thrust/random.h>
+#include <stdlib.h>
+#include <thrust/transform.h>
+#include <stdio.h>
+#include <boost/math/special_functions/binomial.hpp>
 
-//#include <stdio.h>
-using namespace std; 
-using namespace thrust;
+using namespace std;
 
-/*
-int randnum() { 
-	return rand() % 20;
+__device__
+int next_comb(int *comb, int m, int n){
+    int i = m - 1;
+    ++comb[i];
+    
+    while((i >= 0) && (comb[i] >= n - m + 1 - i)){
+        --i;
+        ++comb[i];
+    }
+    if(comb[0] == 1){
+        return 0;
+    }
+    for(i = i + 1; i < m; ++i){
+        comb[i] = comb[i-1] + 1;
+    }
+    return 1;
 }
 
-struct combination{
-
-};
-void combn(float *x, int n, int m, float *result){
-	//copy host to device
-	host_vector<float> hx(x, x+n);
-
-	device_vector<float> dx = hx;
-	device_vector<float> dr = (result, result + (n * m));
-
-	copy(dr.begin(), dr.end(), result);
-
-} */
-
-struct mult
-{
+__device__
+void find_comb(int idx, int *x, int m, int n){
+    //printf("%d, %d\n", i, x[i]);
+    //printf("m = %d\n", m);
+    int *comb = new int[m];
+    for(int i = 0; i < m; i++){
+        comb[i] = i;
+    }
+    int new_n= n - idx;
     
+    /*
+     printf("the x inside = ");
+     for(int i = 0; i < n; i++){
+     printf("%d ", x[i]);
+     }
+     printf("\n");
+     */
+    //	printf("	%d %d %d\n", comb[0], comb[1], comb[2]);
+    printf("index %d has n = %d\n", idx, new_n);
+    
+    printf("ANSWER %d %d %d\n", x[comb[0] + idx], x[comb[1]+idx], x[comb[2]+idx]);
+    printf("after 1st one %d %d %d\n", comb[0], comb[1], comb[2]);
+    
+    //	while(next_comb(comb, m, new_n)){
+    //		printf("inside whiel?");
+    //		printf("%d %d %d\n", x[comb[0]], x[comb[1]], x[comb[2]]);
+    //		printf("		%d %d %d\n", comb[0], comb[1], comb[2]);
+    //	}
+    
+    while(true){
+        printf(" inside comb is: %d %d %d, i = nothing\n", comb[0], comb[1], comb[2]);
+        int i = m - 1;
+        ++comb[i];
+        
+        printf("	after ++comb, comb is %d %d %d, i = %d\n", comb[0], comb[1], comb[2], i);
+        while((i >= 0) && (comb[i] >= new_n - m + 1 + i)){
+            --i;
+            ++comb[i];
+        }
+        printf("	after while, comb is %d %d %d, i = %d\n", comb[0], comb[1], comb[2], i);
+        if(comb[0] == 1){
+            break;
+        }
+        printf("	after if, comb is %d %d %d, i = %d\n", comb[0], comb[1], comb[2], i);
+        for(i = i + 1; i < m; ++i){
+            comb[i] = comb[i-1] + 1;
+        }
+        printf("	after for, comb is %d %d %d, i = %d\n", comb[0], comb[1], comb[2], i);
+        //return 1;
+        
+        printf("ANSWER %d %d %d\n", x[comb[0]+idx], x[comb[1]+idx], x[comb[2]+idx]);
+    }
+}
+
+
+struct comb {
     
     const thrust::device_vector<int>::iterator x;
-    const thrust::device_vector<int>::iterator m;
-    int *x1,*m1;
+    const thrust::device_vector<int>::iterator r;//might use this to store result?
     int n;
+    int m;
+    int *x_ptr, *r_ptr;
+    int *comb_arr;
     
-    mult(thrust::device_vector<int>::iterator _x,
-         thrust::device_vector<int>::iterator _m,
-         int _n):
-        x(_x),m(_m),n(_n)
+    comb(thrust::device_vector<int>::iterator _x, thrust::device_vector<int>::iterator _r, int _n, int _m):
+    x(_x),
+    r(_r),
+    n(_n),
+    m(_m)
     {
-        x1 = thrust::raw_pointer_cast(&x[0]);
-        m1 = thrust::raw_pointer_cast(&m[0]);
+        x_ptr = thrust::raw_pointer_cast(&x[0]);
+        r_ptr = thrust::raw_pointer_cast(&r[0]);
     }
     
-    // each thread operates on a different ith element of the x array
-    __device__ void operator() (const int i)
+    __device__
+    void operator()(int i)
     {
-        //for (int j=0; j<n; j++)
-        {
-            
-        }
-        
-        m1[i]=2*x[i];
+        if(i <= n - m)
+            //	printf("%d ", i);
+            find_comb(i, x_ptr, m, n);
     }
 };
 
-void test(int *x, int *m, int n){
+void combn(int*x, int n, int m, int *comb_arr, int *result, int nCm){
+    //void combn(int *x, int n, int m, vector<int> result){
     
-    // allocate host vector
-    thrust::host_vector<int>hx(x,x+n);
-    thrust::host_vector<int>hm(m,m+n);
+    thrust::device_vector<int> d_x(x, x+n);
+    thrust::device_vector<int> d_r(result, result + (nCm * m));
     
-    // allocate device vector
-    thrust::device_vector<int>dx=hx;
-    thrust::device_vector<int>dm(m,m+n);
+    /*
+     thrust::device_vector<int> d_c(comb_arr, comb_arr + m);
+     for(int i = 0; i < m; i++){
+     printf("%d %d %d", comb_arr[0], comb_arr[1], comb_arr[2]);
+     }
+     */
     
-  /*   thrust::counting_iterator<int>begin(0);
-    thrust::counting_iterator<int>end=begin+n;
-    thrust::for_each(begin,end,mult(dx.begin(),dm.begin(),n));
-    
-    // transfer data back to host
-    thrust::copy(dm.begin(),dm.end(),hm.begin());
-    
-    
-    // For testing purposes, printing out the different arrays:
-    
-     cout << "HX: " << endl;
-     // check what's in hx
-     for(int i = 0; i < hx.size(); i++)
-     cout << hx[i] << ", ";
-     
-    cout << endl << "HM: " << endl << endl;
-     // check output vector
-     for(int i = 0; i < hm.size(); i++)
-     cout << hm[i] << ", "; */
-     
-
+    thrust::counting_iterator<int> begin(0);
+    thrust::counting_iterator<int> end = begin + n;
     
     
+    //	thrust::transform(begin, end, d_r.begin(), comb(d_x.begin(), d_r.begin(), n, m));
+    thrust::for_each(begin, end, comb(d_x.begin(), d_r.begin(), n, m));
+    
+    //thrust::copy(d_r.begin(), d_r.end(), result);
 }
 
-int main(){
-	int n = 5;
-	int m = 3;
 
-    int *hx = (int *) malloc(n*sizeof(int));
-    int *hm = (int *) malloc(n*sizeof(int));
+
+int main(){
+    int n = 5;
+    int m = 3;
     
+    int x[n];
+    int *result;
+    
+    int nCm = boost::math::binomial_coefficient<double>(n, m);
+    result = new int[nCm * m];
+    
+    int *pos = new int[n];	// keeps track of the position in output
+    
+    cout << "x = ";
     for(int i=0; i<n; i++)
     {
-        hx[i] = i;
-		cout << hx[i] << " ";
-	}
-	cout << endl;
-
-	//combn(x, n, m);
+        x[i] = rand() % 5;
+        cout << x[i] << " ";
+    }
+    cout << endl;
     
-    test(hx,hm,n);
-
-	return 0;
-
-
+    int *comb_arr = new int[m];
+    for(int i = 0; i < m; i++){
+        comb_arr[i] = i;
+    }
+    
+    int tmp_n = n;
+    int k = 0;
+    for(int i = 0; i < (n-m+1); i++){
+        pos[i] = boost::math::binomial_coefficient<double>(tmp_n - i - 1, m-1);
+        k++;
+    }
+    //	cout << "nCm = " << nCm << endl;
+    //	cout << "k = " << k << endl;
+    //	cout << "n-m+1 = " << n-m+1 << endl;
+    for(int i = 0; i < n-m+1; i++){
+        cout << "pos[" << i << "] = " << pos[i] << endl;
+    } 
+    
+    //combn(x, n, m, result);   
+    //	combn(x, n, m, comb_arr, result, nCfm);
+    
+    cout << "result = ";
+    for(int i = 0; i < n; i++){
+        cout << result[i] << " ";
+    }
+    cout << endl;
+    return 0;
+    
+    
 }
